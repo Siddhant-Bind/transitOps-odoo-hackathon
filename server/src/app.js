b4150b397f3c272f58a457eb551aae11a8892deb
Saumil Bhatkar <bhatkarsaumil19@gmail.com>
2026-07-12 12:02:05 +0530

    added db

---
+++ server/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import pool from "./config/db.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+
+    try {
+
+        const [rows] = await pool.query(
+            "SELECT NOW() AS currentTime"
+        );
+
+        res.json({
+            success: true,
+            message: "Database Connected Successfully",
+            time: rows[0].currentTime
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Database Connection Failed"
+        });
+
+    }
+
+});
+
+export default app;
